f8dc6e18f50c2afc6fb52dcd36af78180e4fc9ce
Guilherme-Sinapse <guilherme.lourenco@sinapse-ia.com.br>
2024-02-16 12:05:45 -0300

    sql file

---
+++ sql.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS TestePHP CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ 
+USE TestePHP;
+ 
+CREATE TABLE pessoa (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+);
+
+
+
+CREATE TABLE contato (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pessoa_id INT,
+    email VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+    telefone VARCHAR(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+    whatsapp VARCHAR(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+    FOREIGN KEY (pessoa_id) REFERENCES pessoa(id)
+    ON DELETE CASCADE
+);
